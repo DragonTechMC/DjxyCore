64fe092eea569049e73dded74414e932ffd123f6
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-04-25 19:26:04 -0400

    Contains all the permissions of the plugin.

---
+++ src/main/java/io/github/djxy/core/Permissions.java
@@ -0,0 +1,10 @@
+package io.github.djxy.core;
+
+/**
+ * Created by Samuel on 2016-04-24.
+ */
+public class Permissions {
+
+    public static final String DEFAULT_SET_LANGUAGE = "djxycore.commands.admin.set.default.language";
+
+}
